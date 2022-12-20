dde7d27dcf23457b1d748c26b08c1647ae58dbdb
Eyesonjune18 <lowellashton@gmail.com>
2022-12-19 22:25:57 -0800

    Added move pattern checks, although the pawn one does not work as intended.

--- src/chess_piece.rs
+++ src/chess_piece.rs
@@ -1,5 +1,5 @@
-use crate::MoveValidity;
 use crate::ChessMove;
+use crate::MoveValidity;
 
 #[derive(Clone, Copy)]
 pub struct ChessPiece {
@@ -33,10 +33,11 @@ impl ChessPiece {
         }
     }
 
+    // Gets the Unicode character representing the piece
     pub fn get_piece_char(&self) -> char {
         use ChessPieceColor::*;
         use ChessPieceKind::*;
-        
+
         match (&self.color, &self.kind) {
             (White, Pawn) => '♟',
             (White, Rook) => '♜',
@@ -53,44 +54,85 @@ impl ChessPiece {
         }
     }
 
-    pub fn can_move_to(&self, queried_move: ChessMove) -> MoveValidity {
+    // Checks whether the piece can move to the given square, based only on its movement patterns
+    pub fn can_make_move(&self, queried_move: &ChessMove) -> MoveValidity {
+        let change_in_x: u32 = (queried_move.destination().x() as i32 - queried_move.source().x() as i32).abs() as u32;
+        let change_in_y_unadjusted = queried_move.destination().y() as i32 - queried_move.source().y() as i32;
+        let change_in_y: u32 = change_in_y_unadjusted.abs() as u32;
+
+        let vertical_or_horizontal_pattern = (change_in_x == 0 && change_in_y != 0) ^ (change_in_x != 0 && change_in_y == 0);
+        let diagonal_pattern = change_in_x == change_in_y;
+
+        use ChessPieceKind::*;
+        use ChessPieceColor::*;
+
         match self.kind {
-            ChessPieceKind::Pawn => {
+            Pawn => {
+                // Pawn y-axis movement rules are inverted for black pieces,
+                // then adjusted to two squares intead of one for the first move
+                // This code is a bit verbose, but it's also more readable than boolean arithmetic
+                let pawn_standard_move = {
+                    // TODO: En passant is forced on first move, make this allow either
+                    change_in_x == 0 && change_in_y_unadjusted == match (self.color, self.move_count) {
+                        (White, 0) => 2,
+                        (White, _) => 1,
+                        (Black, 0) => -2,
+                        (Black, _) => -1,
+                    }
+                };
+
+                let pawn_capture_move = {
+                    change_in_x == 1 && change_in_y_unadjusted == match self.color {
+                        White => 1,
+                        Black => -1,
+                    }
+                };
+
                 MoveValidity {
-                    standard: true,
-                    capture: true,
+                    standard: pawn_standard_move,
+                    capture: pawn_capture_move,
                 }
-            }
-            ChessPieceKind::Rook => {
+            },
+            Rook => {
+                let rook_move = vertical_or_horizontal_pattern;
+
+                MoveValidity {                
+                    standard: rook_move,
+                    capture: rook_move,
+                }
+            },
+            Knight => {
+                let knight_move = (change_in_x == 1 && change_in_y == 2) ^ (change_in_x == 2 && change_in_y == 1);
+
                 MoveValidity {
-                    standard: true,
-                    capture: true,
+                    standard: knight_move,
+                    capture: knight_move,
                 }
-            }
-            ChessPieceKind::Knight => {
+            },
+            Bishop => {
+                let bishop_move = diagonal_pattern;
+
                 MoveValidity {
-                    standard: true,
-                    capture: true,
+                    standard: bishop_move,
+                    capture: bishop_move,
                 }
-            }
-            ChessPieceKind::Bishop => {
+            },
+            Queen => {
+                let queen_move = vertical_or_horizontal_pattern ^ diagonal_pattern;
+
                 MoveValidity {
-                    standard: true,
-                    capture: true,
+                    standard: queen_move,
+                    capture: queen_move,
                 }
-            }
-            ChessPieceKind::Queen => {
+            },
+            King => {
+                let king_move = (change_in_x <= 1 && change_in_y <= 1) && !(change_in_x == 0 && change_in_y == 0);
+                
                 MoveValidity {
-                    standard: true,
-                    capture: true,
+                    standard: king_move,
+                    capture: king_move,
                 }
-            }
-            ChessPieceKind::King => {
-                MoveValidity {
-                    standard: true,
-                    capture: true,
-                }
-            }
+            },
         }
     }
 }
