2e0a9a7be97f6b85ce24d6f17a1afcd93861d329
Eyesonjune18 <lowellashton@gmail.com>
2022-12-19 19:56:15 -0800

    Added basic move input system for testing.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
+mod chess_gamestate;
 mod chess_move;
 mod chess_piece;
 mod chess_point;
 
+pub use chess_gamestate::ChessGamestate;
 pub use chess_move::ChessMove;
+pub use chess_move::MoveValidity;
 pub use chess_piece::ChessPiece;
+pub use chess_piece::ChessPieceKind;
+pub use chess_piece::ChessPieceColor;
 pub use chess_point::ChessPoint;
 
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    clear();
+
+    let mut game = ChessGamestate::new();
+
+    game.print_board();
+
+    loop {
+        print("\nEnter a move: ");
+
+        let mut user_inputted_move = String::new();
+        std::io::stdin().read_line(&mut user_inputted_move).unwrap();
+        let move_to_make = ChessMove::from(&user_inputted_move);
+
+        clear();
+
+        game.move_piece(move_to_make);
+        game.print_board();
+    }
+}
+
+fn print(output: &str) {
+    print!("{}", output);
+    std::io::stdout().flush().unwrap();
+}
+
+fn clear() {
+    print!("\x1B[2J\x1B[1;1H");
 }
